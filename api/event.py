12cb622454ffce40514b7ebe349c5180f41ffa58
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2024-05-18 23:48:30 +0300

    key changes

--- api/event.py
+++ api/event.py
@@ -1,16 +1,16 @@
 import requests
 import json
+import headers
 
 
 def main():
 
     base_url = "https://ehms.myclub.fi/api/"
-    headers = {"X-myClub-token": "bb8049d99d974aa7793db6be20442d6d"}
     event_url = "events/6582444"  # group 48332
 
     full_url = base_url + event_url
 
-    response = requests.get(full_url, headers=headers)
+    response = requests.get(full_url, headers=headers.headers)
 
     content = json.loads(response.content)
 
--- api/groups.py
+++ api/groups.py
@@ -1,15 +1,15 @@
 import requests
 import json
+import headers
 
 
 def main():
 
     base_url = "https://ehms.myclub.fi/api/"
-    headers = {"X-myClub-token": "bb8049d99d974aa7793db6be20442d6d"}
 
     full_url = base_url + "groups"
 
-    response = requests.get(full_url, headers=headers)
+    response = requests.get(full_url, headers=headers.headers)
 
     content = json.loads(response.content)
     print(json.dumps(content, indent=2))
