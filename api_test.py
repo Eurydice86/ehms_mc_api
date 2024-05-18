12fbc4ca366a3f0a471140272abad580543a4e80
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2024-05-18 23:43:46 +0300

    key

--- api_test.py
+++ api_test.py
@@ -6,7 +6,6 @@ import datetime
 def main():
 
     base_url = "https://ehms.myclub.fi/api/"
-    headers = {"X-myClub-token": "bb8049d99d974aa7793db6be20442d6d"}
 
     now = datetime.datetime.now()
     last_week = now - datetime.timedelta(days=7)
@@ -16,9 +15,7 @@ def main():
 
     full_url = base_url + "events"
 
-    params = {"group_id": "28112",
-              "start_date": last_week,
-              "end_date": now}
+    params = {"group_id": "28112", "start_date": last_week, "end_date": now}
 
     response = requests.get(full_url, headers=headers, params=params)
 
