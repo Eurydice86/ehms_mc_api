05d468a8c213fcd30321fd38e17bfca28e489ab0
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2024-05-29 16:07:16 +0300

    added courses

--- api/data/make_db.sql
+++
@@ -1,7 +0,0 @@
-.separator ","
-.import presences.csv presences
-.import events.csv events
-.import members.csv members
-.import venues.csv venues
-.import groups.csv groups
-.import memberships.csv memberships
